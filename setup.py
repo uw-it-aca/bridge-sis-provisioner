da6876f924d2a8bacadb40bc0fb9d407dec8bc60
Jim Laney <jlaney@uw.edu>
2016-06-06 12:03:41 -0700

    django and travis files

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='bridge-sis-provisioner',
+    version='1.0',
+    packages=['sis_provisioner'],
+    include_package_data=True,
+    install_requires = [
+        'setuptools',
+        'django',
+        'nameparser>=0.2.8'
+    ],
+    license='Apache License, Version 2.0',  # example license
+    description='An application that provisions UW user data to Bridge',
+    long_description=README,
+    url='https://github.com/uw-it-aca/bridge-sis-provisioner',
+    author = "UW-IT ACA",
+    author_email = "aca-it@uw.edu",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
