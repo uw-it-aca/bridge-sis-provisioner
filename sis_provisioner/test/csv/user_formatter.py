05d0a5523c58da9052c23644c5d5644893143db0
Fang Lin <fanglin@uw.edu>
2016-07-18 16:22:50 -0700

    Finalized attributes for importing csv

--- sis_provisioner/test/csv/user_formatter.py
+++ sis_provisioner/test/csv/user_formatter.py
@@ -9,60 +9,70 @@ from sis_provisioner.dao.user import create_user
 class TestUserFormatter(TransactionTestCase):
 
     def test_get_headers(self):
+        self.assertEqual(len(get_headers()), 9)
         self.assertEqual(','.join(get_headers()),
-                         ("Unique ID,Regid,Name,Email," +
+                         ("UNIQUE ID,NAME,EMAIL,regid," +
+                          "alumni,employee,faculty,staff,student"))
+
+        self.assertEqual(len(get_headers(include_emp_data=True)), 10)
+        self.assertEqual(','.join(get_headers(include_emp_data=True)),
+                         ("UNIQUE ID,NAME,EMAIL,regid," +
                           "alumni,employee,faculty,staff,student," +
-                          "emp_status,emp_home_dept_name,emp_campus_code," +
-                          "emp_home_college_code,emp_home_dept_code," +
-                          "student_dept_name"))
+                          "emp home campus"))
 
     def test_get_attr_list(self):
         with self.settings(RESTCLIENTS_PWS_DAO_CLASS=FPWS):
 
             user = create_user('staff')
-            user_attr_list = get_attr_list(user)
+            user_attr_list = get_attr_list(user, include_emp_data=True)
 
-            self.assertEqual(len(user_attr_list), 15)
+            self.assertEqual(len(user_attr_list), 10)
             self.assertEqual(user_attr_list[0],
                              "staff@washington.edu")
             self.assertEqual(user_attr_list[1],
-                             "10000000000000000000000000000001")
-            self.assertEqual(user_attr_list[2],
                              "Staff, James Average")
-            self.assertEqual(user_attr_list[3],
+            self.assertEqual(user_attr_list[2],
                              "staff@uw.edu")
+            self.assertEqual(user_attr_list[3],
+                             "10000000000000000000000000000001")
             self.assertEqual(user_attr_list[4], "y")
             self.assertEqual(user_attr_list[5], "y")
             self.assertEqual(user_attr_list[6], "n")
             self.assertEqual(user_attr_list[7], "y")
             self.assertEqual(user_attr_list[8], "n")
-            self.assertEqual(user_attr_list[9], "S")
-            self.assertEqual(user_attr_list[10], "LIBRARY")
-            self.assertEqual(user_attr_list[11], "2")
-            self.assertEqual(user_attr_list[12], "207")
-            self.assertEqual(user_attr_list[13], "2070001000")
-            self.assertEqual(user_attr_list[14], "")
+            self.assertEqual(user_attr_list[9], "Seattle")
 
             user = create_user('faculty')
-            user_attr_list = get_attr_list(user)
+            user_attr_list = get_attr_list(user, include_emp_data=True)
 
-            self.assertEqual(len(user_attr_list), 15)
+            self.assertEqual(len(user_attr_list), 10)
             self.assertEqual(user_attr_list[0],
                              "faculty@washington.edu")
             self.assertEqual(user_attr_list[1],
-                             "10000000000000000000000000000005")
-            self.assertEqual(user_attr_list[2],
                              "Faculty, James")
-            self.assertEqual(user_attr_list[3],
+            self.assertEqual(user_attr_list[2],
                              "faculty@uw.edu")
+            self.assertEqual(user_attr_list[3],
+                             "10000000000000000000000000000005")
             self.assertEqual(user_attr_list[4], "y")
             self.assertEqual(user_attr_list[5], "y")
             self.assertEqual(user_attr_list[6], "y")
             self.assertEqual(user_attr_list[7], "n")
             self.assertEqual(user_attr_list[8], "n")
-            self.assertEqual(user_attr_list[9], "R")
-            self.assertEqual(user_attr_list[10], "PHYSICS GOF")
-            self.assertEqual(user_attr_list[11], "2")
-            self.assertEqual(user_attr_list[12], "254")
-            self.assertEqual(user_attr_list[13], "2540574070")
-            self.assertEqual(user_attr_list[14], "")
+            self.assertEqual(user_attr_list[9], "Seattle")
+
+            user = create_user('botgrad')
+            user_attr_list = get_attr_list(user, include_emp_data=True)
+
+            self.assertEqual(len(user_attr_list), 10)
+            self.assertEqual(user_attr_list[0],
+                             "botgrad@washington.edu")
+            self.assertEqual(user_attr_list[9], "Bothell")
+
+            user = create_user('tacgrad')
+            user_attr_list = get_attr_list(user, include_emp_data=True)
+
+            self.assertEqual(len(user_attr_list), 10)
+            self.assertEqual(user_attr_list[0],
+                             "tacgrad@washington.edu")
+            self.assertEqual(user_attr_list[9], "Tacoma")
