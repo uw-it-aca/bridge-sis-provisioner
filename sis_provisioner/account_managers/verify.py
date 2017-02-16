0492b76202657cc02d264af89a3b2250f38dece2
Fang Lin <fanglin@uw.edu>
2017-02-15 18:23:14 -0800

    handle the case db user not in bridge

--- sis_provisioner/account_managers/verify.py
+++ sis_provisioner/account_managers/verify.py
@@ -4,6 +4,7 @@ in the last load, re-apply it.
 """
 
 import logging
+from restclients.exceptions import DataFailureException
 from sis_provisioner.dao.bridge import get_bridge_user_object
 from sis_provisioner.account_managers import fetch_users_from_db
 
@@ -13,21 +14,41 @@ logger = logging.getLogger(__name__)
 
 def set_bridge_ids():
     total = 0
+    set_bridge_id_total = 0
+
     for uw_bri_user in fetch_users_from_db(logger):
-        if not uw_bri_user.has_bridge_id():
+
+        if uw_bri_user.has_bridge_id() and\
+           (uw_bri_user.no_action() or\
+            uw_bri_user.disabled or\
+            uw_bri_user.has_terminate_date()):
+            continue
+
+        try:
             bridge_user = get_bridge_user_object(uw_bri_user)
-            if bridge_user is not None and\
-                    bridge_user.netid == uw_bri_user.netid:
-                uw_bri_user.set_bridge_id(bridge_user.bridge_id)
-                logger.info("Set bridge id for %s" % uw_bri_user)
+
+            if bridge_user is not None:
+
+                if bridge_user.netid != uw_bri_user.netid:
+                    logger.error("Bridge and Local Netid not match: %s %s" %
+                                 (bridge_user, uw_bri_user))
+                    continue
+
+                if not uw_bri_user.has_bridge_id():
+                    uw_bri_user.set_bridge_id(bridge_user.bridge_id)
+                    logger.info("Set bridge id for %s" % uw_bri_user)
+                    set_bridge_id_total = set_bridge_id_total + 1
+
+                uw_bri_user.save_verified(upd_last_visited=False)
                 total = total + 1
-            else:
-                logger.error("Get bridge user (%s) find none!" % uw_bri_user)
-                continue
 
-        if not uw_bri_user.no_action() and\
-                not uw_bri_user.disabled and\
-                not uw_bri_user.has_terminate_date():
-            uw_bri_user.save_verified(upd_last_visited=False)
+        except DataFailureException as ex:
+            logger.error("GET %s ==> %s" % (uw_bri_user, ex))
+            if ex.status == 404:
+                logger.info("Not in Bridge, remove local record %s" %
+                            uw_bri_user)
+                uw_bri_user.delete()
 
-    return total
+    logger.info("Set bridge ids for %d users" % set_bridge_id_total)
+    logger.info("Verified %d users" % total)
+    return set_bridge_id_total
--- sis_provisioner/test/account_managers/verify.py
+++ sis_provisioner/test/account_managers/verify.py
@@ -12,19 +12,34 @@ class TestUserVerifier(TransactionTestCase):
     def test_set_bridge_ids_for_existing_users(self):
         self.assertEqual(set_bridge_ids(), 0)
 
-        user = UwBridgeUser(netid='javerage',
-                            regid='9136CCB8F66711D5BE060004AC494FFE',
-                            last_visited_at=get_now(),
-                            email='javerage@uw.edu',
-                            first_name='James',
-                            last_name='Changed')
-        user.save()
-        user1 = UwBridgeUser(netid='leftuw',
-                             regid='...',
-                             last_visited_at=get_now(),
-                             email='leftuw@uw.edu',
-                             first_name='Left',
-                             last_name='UW')
-        user1.save()
+        UwBridgeUser(netid='javerage',
+                     regid='9136CCB8F66711D5BE060004AC494FFE',
+                     last_visited_at=get_now(),
+                     email='javerage@uw.edu',
+                     first_name='James',
+                     last_name='Average').save()
+
+        UwBridgeUser(netid='leftuw',
+                     regid="B814EFBC6A7C11D5A4AE0004AC494FFE",
+                     last_visited_at=get_now(),
+                     email='leftuw@uw.edu',
+                     first_name='Left',
+                     last_name='UW').save()
+
+        UwBridgeUser(netid='staff',
+                     regid="10000000000000000000000000000001",
+                     bridge_id=196,
+                     last_visited_at=get_now(),
+                     email='staff@uw.edu',
+                     first_name='Staff',
+                     last_name='UW',
+                     action_priority=1).save()
+
+        UwBridgeUser(netid='toremove',
+                     regid='...',
+                     last_visited_at=get_now(),
+                     email='leftuw@uw.edu',
+                     first_name='Not In',
+                     last_name='Bridge').save()
 
         self.assertEqual(set_bridge_ids(), 2)
