e9c07fa522ec7ebeb21f0c413833662fef26291b
Fang Lin <fanglin@uw.edu>
2019-04-04 16:09:26 -0700

    upd travis-ci urls

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -3,8 +3,5 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^support', include('userservice.urls')),
-    url(r'^support/', include('userservice.urls')),
-    url(r'^restclients', include('restclients.urls')),
     url(r'^', include('sis_provisioner.urls')),
 ]
