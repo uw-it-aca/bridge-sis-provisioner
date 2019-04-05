be120b01e259edefaaba30c445399defd768374b
Fang Lin <fanglin@uw.edu>
2019-04-05 11:23:16 -0700

    clean up

--- sis_provisioner/dao/__init__.py
+++ sis_provisioner/dao/__init__.py
@@ -1,6 +1,1 @@
 from restclients_core.exceptions import DataFailureException, InvalidNetID
-from uw_pws import DAO as PWS_DAO
-
-
-def is_using_file_dao():
-    return SWS_DAO.get_implementation().is_mock()
