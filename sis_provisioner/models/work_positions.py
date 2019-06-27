c795bdeae8f443a997fe628b54fd957cb495d5e9
Fang Lin <fanglin@uw.edu>
2019-06-27 15:22:15 -0700

    add models/work_positions

---
+++ sis_provisioner/models/work_positions.py
@@ -0,0 +1,18 @@
+from uw_bridge.models import BridgeCustomField
+
+
+WORK_POSITION_FIELDS = []
+WORK_POSITION_FIELDS.append([BridgeCustomField.POS1_BUDGET_CODE,
+                             BridgeCustomField.POS1_JOB_CLAS,
+                             BridgeCustomField.POS1_JOB_CODE,
+                             BridgeCustomField.POS1_LOCATION,
+                             BridgeCustomField.POS1_ORG_CODE,
+                             BridgeCustomField.POS1_ORG_NAME,
+                             BridgeCustomField.POS1_UNIT_CODE])
+WORK_POSITION_FIELDS.append([BridgeCustomField.POS2_BUDGET_CODE,
+                             BridgeCustomField.POS2_JOB_CLAS,
+                             BridgeCustomField.POS2_JOB_CODE,
+                             BridgeCustomField.POS2_LOCATION,
+                             BridgeCustomField.POS2_ORG_CODE,
+                             BridgeCustomField.POS2_ORG_NAME,
+                             BridgeCustomField.POS2_UNIT_CODE])
