26641d405df13f415f41cf60420a25f1f59ea298
Fang Lin <fanglin@uw.edu>
2019-08-06 19:29:01 -0700

    bug fix in uw_bridge, in sync_authors

--- sis_provisioner/management/commands/sync_authors.py
+++ sis_provisioner/management/commands/sync_authors.py
@@ -19,13 +19,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         timer = Timer()
         logger.info("Start at {0}".format(datetime.now()))
+        loader = AuthorChecker(BridgeWorker())
         try:
-            loader = AuthorChecker(BridgeWorker())
             loader.load()
-            logger.info("{0:d} users updated", loader.get_updated_count())
-            if loader.has_error():
-                logger.error("Errors: {0}".format(loader.get_error_report()))
         except Exception as ex:
             logger.error(str(ex))
         finally:
             log_resp_time(logger, "Sync authors", timer)
+
+        logger.info("{0:d} users updated".format(loader.get_updated_count()))
+        if loader.has_error():
+            logger.error("Errors: {0}".format(loader.get_error_report()))
