bad1d0165ddaacae903e891da57b186287de66d1
Fang Lin <fanglin@uw.edu>
2021-07-23 14:03:27 -0700

    clean up

--- sis_provisioner/dao/__init__.py
+++ sis_provisioner/dao/__init__.py
@@ -3,7 +3,6 @@
 
 from datetime import timedelta
 import logging
-import os
 from restclients_core.exceptions import (
     DataFailureException, InvalidNetID, InvalidRegID)
 from sis_provisioner.models import get_now
@@ -18,34 +17,3 @@ def get_dt_from_now(duration):
 
 def changed_since_str(duration):
     return get_dt_from_now(duration).strftime("%Y-%m-%d %H:%M:%S")
-
-
-def read_gws_cache_file(path):
-    user_set = set()
-    if os.path.isfile(path):
-        with open(path, 'r', encoding='utf8') as data_source:
-            for line in data_source:
-                try:
-                    user_set.add(line.rstrip())
-                except Exception as ex:
-                    logger.error("{} read_gws_cache_file({}), Skip {}".format(
-                        str(ex), path, line))
-    return user_set
-
-
-def rename_file(path):
-    if os.path.isfile(path):
-        rname = "{}.{}".format(path, get_now().strftime("%Y%m%d.%H%M"))
-        os.rename(path, rname)
-
-
-def write_gws_cache_file(path, user_set):
-    rename_file(path)
-    f = open(path, "w")
-    for netid in sorted(list(user_set)):
-        try:
-            f.write("{}\n".format(netid))
-        except Exception as ex:
-            logger.error("{} write_gws_cache_file({}), Skip {}".format(
-                str(ex), path, netid))
-    f.close()
