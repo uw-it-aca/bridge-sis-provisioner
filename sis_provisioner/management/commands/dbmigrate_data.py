4ec876f76300c317257e89692b61c97d8ceb8831
Fang Lin <fanglin@uw.edu>
2023-07-27 12:43:35 -0700

    Fix file path

--- sis_provisioner/management/commands/dbmigrate_data.py
+++ sis_provisioner/management/commands/dbmigrate_data.py
@@ -6,9 +6,10 @@ import os
 from django.db import connections
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
+from sis_provisioner.util.settings import get_csv_file_path_prefix
+from sis_provisioner.models import UwAccount
 
 
-fixture_file = '/app/mysql_data_fixture.json'
 logger = logging.getLogger(__name__)
 
 
@@ -19,8 +20,10 @@ class Command(BaseCommand):
             'action', choices=['all', 'prep', 'dump', 'load', 'inspect'])
 
     def handle(self, *args, **options):
+        self.fixture_file = os.path.join(
+            get_csv_file_path_prefix(), 'mysql_data_fixture.json')
+
         self.action = options['action']
-        os.remove(fixture_file)
 
         if self.action == 'dump':
             self.dump_mysql_data()
@@ -37,23 +40,26 @@ class Command(BaseCommand):
             self.inspect_postgresqldb()
 
         # Cleanup: Delete the local fixture file
-        # os.remove(fixture_file)
+        # os.remove(self.fixture_file)
 
     def before_dump(self):
         with connections['mysql'].cursor() as cursor:
             cursor.execute("DELETE FROM django_session")
 
     def dump_mysql_data(self):
+        os.remove(self.fixture_file)
         try:
-            call_command('dumpdata', '--database=mysql', output=fixture_file)
+            call_command(
+                'dumpdata', '--database=mysql', output=self.fixture_file)
         except CommandError as e:
             logger.error("Dump table from the MySQL DB: {}".format(e))
 
     def load_postgresdb(self):
         try:
-            call_command('loaddata', fixture_file)
+            call_command('loaddata', self.fixture_file)
         except CommandError as e:
             logger.error("Load data into Postgres DB: {}".format(e))
 
     def inspect_postgresqldb(self):
-        call_command('inspectdb')
+        logger.info("{} UwAccount loaded".format(
+            len(UwAccount.objects.all())))
